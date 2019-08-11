64829cab734a48ded6311214f54296b767c08f63
Michal Odnous <odi.root@gmail.com>
2019-08-11 22:20:30 +0200

    Actually use UUID Primary Keys, migration cleanup.

--- common/models.py
+++ common/models.py
@@ -3,8 +3,11 @@ from uuid import uuid4
 from django.db import models
 
 
-class UUIDMixin(object):
+class UUIDMixin(models.Model):
     """
     Mixing for changing models from integer-based PK to an UUID one.
     """
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    class Meta:
+        abstract = True
