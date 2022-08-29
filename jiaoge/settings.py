7dff2aa319386107bafbb112bc8e4e31d25075a6
Michal Odnous <odiroot@users.noreply.github.com>
2022-08-29 21:30:13 +0100

    Isort fix.

--- jiaoge/settings.py
+++ jiaoge/settings.py
@@ -10,7 +10,6 @@ import environ  # django-environ for 12-factor app compatible settings.
 import sentry_sdk  # Sentry for error reporting.
 from sentry_sdk.integrations.django import DjangoIntegration
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 WSGI_APPLICATION = "jiaoge.wsgi.application"
