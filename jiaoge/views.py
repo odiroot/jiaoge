d2e73444ac5502a06ddda0d0f1981071f2ff153c
Michal Odnous <odiroot@users.noreply.github.com>
2022-09-06 21:16:09 +0100

    Make the administration part less obvious.

--- jiaoge/views.py
+++ jiaoge/views.py
@@ -2,6 +2,6 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 
 
-@staff_member_required
+@staff_member_required(login_url="/code-claim")
 def landing(request):
     return render(request, "landing.html")
